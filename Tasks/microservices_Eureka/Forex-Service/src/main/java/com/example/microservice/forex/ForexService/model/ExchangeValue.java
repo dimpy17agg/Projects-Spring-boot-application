9fe3a000201202bfebfcc190635cc69cf67c5a64
dimpy17agg <dimpy17aggarwal@gmail.com>
2020-09-07 23:23:33 +0530

    Micro service communication via eureka

---
+++ Tasks/microservices_Eureka/Forex-Service/src/main/java/com/example/microservice/forex/ForexService/model/ExchangeValue.java
@@ -0,0 +1,83 @@
+package com.example.microservice.forex.ForexService.model;
+
+import javax.persistence.Entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class ExchangeValue {
+
+	@Id
+	@Column
+	private Long id;
+    @Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+    private String to;
+	@Column
+	private BigDecimal conversionMultiple;
+	@Column  
+	private int port;
+	public ExchangeValue(){
+		
+	}
+	
+
+	public ExchangeValue(Long id, String from, String to,BigDecimal conversionMultiple,int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple=conversionMultiple;
+		this.port=port;
+	}
+
+
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+
+	public int getPort() {
+		return port;
+	}
+
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+}
